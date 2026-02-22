4336e0cbae39965ccc3a30124f0a9aba356cb148
CYoJkoY <Cjy3430525946@outlook.com>
2026-02-22 12:09:28 +0800

    Update debug_menu.gd

--- extensions/debug_menu.gd
+++ extensions/debug_menu.gd
@@ -1,6 +1,11 @@
 extends "res://ui/menus/debug/debug_menu.gd"
 
+var mods_enemies_directories = [
+    "res://mods-unpacked/Yoko-YzTato/content/entities/enemies",
+    "res://mods-unpacked/Yoko-Fantasy/content/entities/enemies",
+]
+
 # =========================== Extension =========================== #
-func _init() -> void:
-    enemies_directories.append("res://mods-unpacked/Yoko-YzTato/content/entities/enemies")
-    enemies_directories.append("res://mods-unpacked/Yoko-Fantasy/content/entities/enemies")
+func _set_menu() -> void:
+    ._set_menu()
+    enemies_directories.append_array(mods_enemies_directories)
