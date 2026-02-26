98562993a8fa24b617f0e5c1128667c8d3cd8f9a
CYoJkoY <Cjy3430525946@outlook.com>
2026-02-26 12:19:29 +0800

    Update character_selection.gd

--- extensions/character_selection.gd
+++ extensions/character_selection.gd
@@ -1,11 +1,8 @@
 extends "res://ui/menus/run/character_selection.gd"
 
-const FONT_RESOURCE = preload("res://resources/fonts/actual/base/font_40_outline_thick.tres")
-var _player_gmo_num: int = ProgressData.settings.yztato_gmo_num
-
 # =========================== Extension =========================== #
 func _get_unlocked_elements(player_index: int) -> Array:
-    var unlocked: Array = ._get_unlocked_elements(player_index)
+    var unlocked: Array =._get_unlocked_elements(player_index)
     unlocked = _yztato_unlock_all_chars(unlocked, player_index)
     
     return unlocked
@@ -13,7 +10,7 @@ func _get_unlocked_elements(player_index: int) -> Array:
 # =========================== Custom =========================== #
 func _yztato_unlock_all_chars(unlocked: Array, player_index: int) -> Array:
     if ProgressData.settings.yztato_unlock_all_chars:
-        var all_unlocked: = []
+        var all_unlocked := []
         for element in _get_all_possible_elements(player_index):
             all_unlocked.append(element.my_id_hash)
         return all_unlocked
